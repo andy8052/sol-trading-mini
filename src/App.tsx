077c2f4a1e5545f21b0b27fed9416d167a8c4931
andy8052 <andychorlian@gmail.com>
2024-09-30 14:32:51 -0400

    evm

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ const App: React.FC = () => {
       if (!username) throw new Error("Username not found");
 
       const pregenWallet = await capsuleClient.createWalletPreGen(
-        WalletType.SOLANA,
+        WalletType.EVM,
         `${username + crypto.randomUUID().split("-")[0]}@test.usecapsule.com`
       );
 
